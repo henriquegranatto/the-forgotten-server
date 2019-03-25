5d16795de8501fbbe57b10a1c75792a710c3ab94
Henrique <henrique.ramires.granatto@gmail.com>
2019-03-25 00:44:59 -0300

    Implementado monster controller; Implementado modelo de scvript; Adicionado modelo ao monster.json; Adicionada rota para monster controller.

---
+++ data/monsters/scripts/modelo.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//      MODELO DA ESTRUTURA PADRÃO DE UM SCRIPT      *\
+//      PARA QUE O SCRIPT FUNCIONE CORRETAMENTE      *\
+//      INFORME NO CAMPO DATA DA SUA REQUISIÇÃO      *\
+//         OS PARÂMETROS INFORMADOS ABAIXO.          *\
+
+exports.execute = async ({cid, item, fromPosition, itemEx, toPosition, isHotkey}) =>
+{
+
+    // TODOS OS SCRIPTS DEVEM RETORNAR ALGUMA INFORMAÇÃO.
+    return {status: 200, messagem: "Modelo de Monster Script Executado com sucesso"}
+}
