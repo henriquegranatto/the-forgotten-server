250b088fb7e3290292632b0b6c9fa38d4596e5be
Henrique <henrique.ramires.granatto@gmail.com>
2019-03-31 17:59:52 -0300

    Adicionada middleware de auth nas rotas Implementado auth middleware Ajustado auth controller

---
+++ app/Middleware/Auth.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Hash = use('Hash')
+
+let account = require('../Controllers/Http/AccountController')
+account = new account()
+
+class Auth 
+{
+  async handle ({ request, response, auth }, next) 
+  {    
+    try
+    {
+      const user = await account.get({publicCode: request.body.publicCode, token: request.body.token})
+      console.log(user)
+      if(user.status == 400) throw user
+      if(!user.data) throw {status: 400, messagem: "Falha na autenticação"} 
+      if(user.data.length != 1) throw {status: 400, messagem: "Falha na autenticação"} 
+      await next()
+    }
+    catch(e)
+    {
+        // RETORNA ALGUM POSSÍVEL ERRO
+        const error = {status: 400, message: "Falha na autenticação", error: {code: "AuthMiddleware.auth", message: e}}
+        response.send(error)
+    }
+  }
+}
+
+module.exports = Auth
