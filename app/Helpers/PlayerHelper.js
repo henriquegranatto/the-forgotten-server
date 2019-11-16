2bcf6c24443b7629999a48d60930ceff2d0b48d6
henriquegranatto <henrique.ramires.granatto@gmail.com>
2019-11-16 17:10:14 -0300

    Implementado helper

---
+++ app/Helpers/PlayerHelper.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const Database = use('Database')
+const server_error = use('App/Controllers/Http/ServerLogErrorController')
+
+class PlayerHelper 
+{
+    async getPlayer (request)
+    {
+        try
+        {
+            const player = await Database.select('*').from('accounts').where({id: request.id})
+            return {status: 200, data: player}
+        }
+        catch(e)
+        {
+            // REGISTRA O ERRO NO BANCO DE DADOS E RETORNA A REQUISIÇÃO
+            const code = await server_error.register("PlayerHelper.getPlayer", JSON.stringify(data), e.toString(), `${__dirname}/${__filename}`)
+            return code
+        }
+    }
+
+    async editPlayer(request)
+    {
+        try
+        {
+            const where = {id: request.id}
+            delete request.id
+
+            const player = await Database.table('accounts').where(where).update(request)
+            return {status: 200}
+        }
+        catch(e)
+        {
+            // REGISTRA O ERRO NO BANCO DE DADOS E RETORNA A REQUISIÇÃO
+            const code = await server_error.register("PlayerHelper.editPlayer", JSON.stringify(request), e.toString(), `${__dirname}/${__filename}`)
+            return code
+        }
+    }
+}
+
+module.exports = new PlayerHelper()
